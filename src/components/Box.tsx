d54ade599b2beee88143c9cd1b210e7ebef18789
nikolayhotim1 <hotim_nikolay@mail.ru>
2023-06-03 16:02:38 +0300

    feature: improve app layout

--- src/components/Box.tsx
+++ src/components/Box.tsx
@@ -1,8 +1,10 @@
+import { twMerge } from 'tailwind-merge'
+
 interface BoxProps {
 	children: React.ReactNode
 	className?: string
 }
 
 export function Box({ children, className }: BoxProps) {
-	return <div>{children}</div>
+	return <div className={twMerge(`bg-neutral-900 rounded-lg h-fit w-full`, className)}>{children}</div>
 }
